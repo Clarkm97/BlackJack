d2ec8c09cb455af2e281caef1e657709153ecc30
Clarkm97 <clark.murdock1@gmail.com>
2021-02-04 13:52:09 -0600

    Added Cards_Owned_By_Players

---
+++ src/main/java/com/sg/blackjack/model/Cards_Owned_By_Players.java
@@ -0,0 +1,22 @@
+package com.sg.blackjack.model;
+
+public class Cards_Owned_By_Players {
+    private int playerId;
+    private int cardId;
+
+    public int getPlayerId() {
+        return playerId;
+    }
+
+    public void setPlayerId(int playerId) {
+        this.playerId = playerId;
+    }
+
+    public int getCardId() {
+        return cardId;
+    }
+
+    public void setCardId(int cardId) {
+        this.cardId = cardId;
+    }
+}
