46b517b719155ee9d988a726d982c61cbde39b40
Robert Villazana <r.villazana@yahoo.com>
2021-02-04 14:01:01 -0600

    Added JDBC dependency to POM.xml and created Game DAO implementation class

---
+++ src/main/java/com/sg/blackjack/data/GameDaoImpl.java
@@ -0,0 +1,29 @@
+package com.sg.blackjack.data;
+
+import com.sg.blackjack.model.Game;
+
+import java.util.List;
+
+public class GameDaoImpl implements GameDao {
+
+
+    @Override
+    public List<Game> getAllGames() {
+        return null;
+    }
+
+    @Override
+    public Game findByGameID(int id) {
+        return null;
+    }
+
+    @Override
+    public boolean updateGame(int id) {
+        return false;
+    }
+
+    @Override
+    public Game addGame(Game newGame) {
+        return null;
+    }
+}
