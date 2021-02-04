45a4a1297056b3b7e8dc680f9f68475469d73981
Matthew Blackman <icecoldsir@yahoo.com>
2021-02-04 14:51:11 -0600

    controller changes

--- src/main/java/com/sg/blackjack/controller/BlackJackController.java
+++ src/main/java/com/sg/blackjack/controller/BlackJackController.java
@@ -1,25 +1,44 @@
 package com.sg.blackjack.controller;
 
+import com.sg.blackjack.data.GameDao;
+import com.sg.blackjack.data.GameDataBaseDao;
 import com.sg.blackjack.model.Game;
 import com.sg.blackjack.model.Player;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/blackjack")
 public class BlackJackController {
 
-    @PostMapping("/begin")
-    public Player beginGame() {
-        // return player?
+    @PostMapping("/begin/{name}")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Game beginGame(@PathVariable String name) {
+
+        //creates game and two players (dealer/player)
+        // return game
         return null;
+    }
+    @GetMapping("/game")
+    public List<Game> allGames(){
+        // return list of all games in database
+        return null;
+    }
+    @GetMapping("/game/{gameId}")
+    public ResponseEntity<Game> getGame(@PathVariable int gameId){
+        // returns 1 game based on given Id
+
+        return null;
+
     }
 
     @PostMapping("/game/{action}") // action = hit or stay
     public Game playGame(@PathVariable String action) {
-        // return game?
+        // returns updated version of game after action
         return null;
     }
+
 }
