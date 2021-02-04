21eabb808b2a8f8724d8af9dd9cab3913bae27fd
Clarkm97 <clark.murdock1@gmail.com>
2021-02-04 10:45:03 -0600

    DataBase Setup

---
+++ blackjack-data.sql
@@ -0,0 +1,16 @@
+use blackjack;
+
+insert into card(cardName, primaryPoints,altPoints) values 
+("Ace",11,1),
+("2",2,2),
+("3",3,3),
+("4",4,4),
+("5",5,5),
+("6",6,6),
+("7",7,7),
+("8",8,8),
+("9",9,9),
+("10",10,10),
+("Jack",10,10),
+("Queen",10,10),
+("King",10,10);
