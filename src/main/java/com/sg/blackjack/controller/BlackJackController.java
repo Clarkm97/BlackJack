15e6facf40049a7873bd62f5cbe2ceea82c0aafc
Matthew Blackman <icecoldsir@yahoo.com>
2021-02-05 09:49:07 -0600

    implementing /game/{gameId} endpoint in controller

--- src/main/java/com/sg/blackjack/controller/BlackJackController.java
+++ src/main/java/com/sg/blackjack/controller/BlackJackController.java
@@ -33,11 +33,14 @@ public class BlackJackController {
         // return list of all games in database
         return gameDao.getAllGames();
     }
-    @GetMapping("/game/{gameId}")
-    public Game getGame(@PathVariable int gameId){
+    @@GetMapping("/game/{gameId}")
+    public ResponseEntity<Game> getGame(@PathVariable int gameId){
         // returns 1 game based on given Id
-        return gameDao.findByGameID(gameId);
-
+        Game result = gameDao.findByGameID(gameId);
+        if (result == null) {
+            return new ResponseEntity(null, HttpStatus.NOT_FOUND);
+        }
+        return ResponseEntity.ok(result);
     }
 
     @PostMapping("/game/{gameId}/{action}") // action = hit or stay
