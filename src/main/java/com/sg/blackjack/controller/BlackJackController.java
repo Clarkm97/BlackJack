fa4c7f9d6b4ae9ea23c20ca1afab9a8b2f4b4bb9
Matthew Blackman <icecoldsir@yahoo.com>
2021-02-05 09:42:19 -0600

    implementing /game/{gameId} endpoint

--- src/main/java/com/sg/blackjack/controller/BlackJackController.java
+++ src/main/java/com/sg/blackjack/controller/BlackJackController.java
@@ -4,6 +4,7 @@ import com.sg.blackjack.data.GameDao;
 import com.sg.blackjack.data.GameDataBaseDao;
 import com.sg.blackjack.model.Game;
 import com.sg.blackjack.model.Player;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,6 +14,9 @@ import java.util.List;
 @RestController
 @RequestMapping("/blackjack")
 public class BlackJackController {
+
+    @Autowired
+    GameDao gameDao;
 
     @PostMapping("/begin/{name}")
     @ResponseStatus(HttpStatus.CREATED)
@@ -30,10 +34,13 @@ public class BlackJackController {
     @GetMapping("/game/{gameId}")
     public ResponseEntity<Game> getGame(@PathVariable int gameId){
         // returns 1 game based on given Id
-
-        return null;
-
+        Game result = gameDao.findByGameID(gameId);
+        if (result == null) {
+            return new ResponseEntity(null, HttpStatus.NOT_FOUND);
+        }
+        return ResponseEntity.ok(result);
     }
+
 
     @PostMapping("/game/{gameId}/{action}") // action = hit or stay
     public Game playGame(@PathVariable int gameId,String action) {
